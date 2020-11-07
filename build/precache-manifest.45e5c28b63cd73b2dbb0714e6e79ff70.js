9b3a1b11e5b5369b914f1a705783f20baf4ee9b0
Jasmine Bailey <lajoye.russell@gmail.com>
2020-11-07 06:01:05 -0500

    commit

---
+++ build/precache-manifest.45e5c28b63cd73b2dbb0714e6e79ff70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f543c9406907d2dc57bf928225a06d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9907e795a4d811cab1e",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "a2b392b94a62587ca027",
+    "url": "/static/css/main.8c316816.chunk.css"
+  },
+  {
+    "revision": "e9907e795a4d811cab1e",
+    "url": "/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "a2b392b94a62587ca027",
+    "url": "/static/js/main.7848e7e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
